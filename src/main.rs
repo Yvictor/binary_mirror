6b959b2739900f941c2685e00d75cf898417c29a
yvictor <yvictor3141@gmail.com>
2024-12-06 12:57:03 +0800

    feat: main usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use binary_mirror::BinaryMirror;
+
+#[repr(C)]
+#[derive(BinaryMirror)]
+pub struct SomePayload {
+    #[bm(type = "str")]
+    company: [u8; 7],
+    #[bm(type = "str")]
+    exh: [u8; 8],
+    #[bm(type = "str")]
+    stkprc1: [u8; 20],
+    #[bm(type = "i32")]
+    ordqty: [u8; 4],
+}
+
 // #[tokio::main]
 // async 
 fn main() {
@@ -10,7 +25,14 @@ fn main() {
         .with_line_number(true)
         .with_level(true)
         .init();
-
+    let payload = SomePayload {
+        company: *b"COMPANY",
+        exh: *b"EXCHANGE",
+        stkprc1: *b"1234.56             ",
+        ordqty: *b"1234",
+    };
+    
+    println!("{:?}", payload);
 //     // 同時運行伺服器和客戶端
 //     tokio::join!(
 //         server::run_query_server(),
